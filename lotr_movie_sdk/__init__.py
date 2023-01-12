bd926f47d165031ad409e4cbc24f33d9a300ea32
shalom.cohen@wunderkind.co <shalom.cohen@wunderkind.co>
2023-01-12 13:05:54 -0500

    initial commit

---
+++ lotr_movie_sdk/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""Lord Of TheRings Movie SDK - Test for LibLab"""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
